9985d10b127e16e4fb57dcc09521fa5d5e18b98f
abnahid <aznahid507@gmail.com>
2024-09-19 06:15:38 +0600

    Lohin Page desing

--- js/features.js
+++ js/features.js
@@ -1,6 +1,6 @@
 // Show the Cash Out
 document
-  .getElementById("tn-show-cash-out")
+  .getElementById("btn-show-cash-out") // corrected from "tn-show-cash-out" to "btn-show-cash-out"
   .addEventListener("click", function (event) {
     // show cash out button clicked
     document.getElementById("cash-out-form").classList.remove("hidden");
@@ -9,11 +9,10 @@ document
     document.getElementById("add-money-form").classList.add("hidden");
   });
 
-// show add money form and hide the cash out form
+// Show Add Money Form and Hide the Cash Out Form
 document
   .getElementById("btn-show-add-money")
   .addEventListener("click", function () {
     document.getElementById("add-money-form").classList.remove("hidden");
-
     document.getElementById("cash-out-form").classList.add("hidden");
   });
